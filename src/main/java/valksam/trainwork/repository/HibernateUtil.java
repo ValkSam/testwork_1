6d889f631256b64bb0969b8801dd5ac25d7b0f3e
ValkSam <do110473sva@gmail.com>
2016-01-30 13:59:09 +0200

    little corrections

--- src/main/java/valksam/trainwork/repository/HibernateUtil.java
+++ src/main/java/valksam/trainwork/repository/HibernateUtil.java
@@ -1,7 +1,9 @@
 package valksam.trainwork.repository;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 /**
  * Created by Valk on 26.01.16.
@@ -9,19 +11,21 @@ import org.hibernate.cfg.Configuration;
 public class HibernateUtil {
     private static SessionFactory sessionFactory;
 
-    private static SessionFactory buildSessionFactory() {
-        SessionFactory sessionFactory = new Configuration().configure("/db/hibernate.cfg.xml")
-                .buildSessionFactory();
-        return sessionFactory;
+    private static void buildSessionFactory() {
+        Configuration configuration = new Configuration();
+        configuration.configure("/db/hibernate.cfg.xml");
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(
+                configuration.getProperties()).build();
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
     }
 
     public static SessionFactory getSessionFactory() {
-        if (sessionFactory == null) sessionFactory = buildSessionFactory();
+        if (sessionFactory == null) buildSessionFactory();
         return sessionFactory;
     }
 
-    public static void shutdown(){
-        getSessionFactory().close();
+    public static void shutdown() {
+        if (sessionFactory != null) getSessionFactory().close();
     }
 
 }
